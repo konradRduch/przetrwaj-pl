36c0626c10ebd04bbf13a2b55cb677244d1c93ae
GargamelJR1 <91876652+GargamelJR1@users.noreply.github.com>
2024-01-03 19:56:52 +0100

    set center to current location

--- src/app/mapka/mapka.component.ts
+++ src/app/mapka/mapka.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-mapka',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./mapka.component.css']
 })
 export class MapkaComponent {
+  center: google.maps.LatLngLiteral = { lat: 50.2889384, lng: 18.6777903 };
 
+  
+
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      }
+    });
+  }
 }
