1e2159d88ab5e4efd6e605855abc407c8c4f7cf1
GargamelJR1 <91876652+GargamelJR1@users.noreply.github.com>
2024-01-06 19:42:31 +0100

    marker title, custom icon

--- src/app/mapka/mapka.component.ts
+++ src/app/mapka/mapka.component.ts
@@ -1,6 +1,17 @@
 import { Component, ViewChild } from '@angular/core';
 import { GoogleMap } from '@angular/google-maps';
 
+interface Marker {
+  position: {
+    lat: number,
+    lng: number,
+  };
+  title: string; // visible on hover
+  options: {
+    icon: string; // url to custom marker icon
+  }
+}
+
 @Component({
   selector: 'app-mapka',
   templateUrl: './mapka.component.html',
@@ -8,7 +19,7 @@ import { GoogleMap } from '@angular/google-maps';
 })
 export class MapkaComponent {
   center: google.maps.LatLngLiteral = { lat: 50.28854410989689, lng: 18.678064408570513 }; // default location - AEI Gliwice
-  markers: { position: { lat: number, lng: number } }[] = [];
+  markers: Marker[] = [];
   @ViewChild('map', { static: true }) map!: GoogleMap;
 
   addMarker(event: google.maps.MapMouseEvent) {
@@ -16,6 +27,10 @@ export class MapkaComponent {
       position: {
         lat: event.latLng!.lat(),
         lng: event.latLng!.lng(),
+      },
+      title: 'Sample title',
+      options: {
+        icon: this.getMarkerUrl('green'),
       }
     })
   }
@@ -27,5 +42,9 @@ export class MapkaComponent {
         lng: position.coords.longitude,
       }
     });
+  }
+
+  getMarkerUrl(color: string): string {
+    return `http://maps.google.com/mapfiles/ms/icons/${color}-dot.png`;
   }
 }
