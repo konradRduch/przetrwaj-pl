bc866a2a48cf9e9f7acafc06e809ccdccf7f998c
LazyyMonster <karol.skiba@op.pl>
2024-01-25 18:28:32 +0100

    adding incident from form, setting current date to creationDate

--- src/app/incident-add-form/incident-add-form.component.ts
+++ src/app/incident-add-form/incident-add-form.component.ts
@@ -1,27 +1,57 @@
 import { Component } from '@angular/core';
 import { LocationService } from '../services/location.service';
+import { IncidentsService } from '../services/incidents.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-incident-add-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './incident-add-form.component.html',
   styleUrl: './incident-add-form.component.css'
 })
+
 export class IncidentAddFormComponent {
   latMarker: any;
   lngMarker: any;
+  incidentTitle: string = "";
+  incidentDescription: string = "";
+  incidentTypeName!: string;
 
   setLocation(lat: number, lng: number){
     this.latMarker = lat
     this.lngMarker = lng
   }
 
-  constructor(private locationService: LocationService) { }
+  constructor(private locationService: LocationService, private incidentService: IncidentsService) {
+   }
 
   ngOnInit() {
     this.locationService.currentLocation.subscribe(location => {
       this.setLocation(location.lat, location.lng);
     });
   }
+
+  addIncident() {
+    let creationDate = new Date();
+    let expirationDate = new Date(creationDate);
+    expirationDate.setMonth(creationDate.getMonth() + 1);
+    const incident = {
+        title: this.incidentTitle,
+        description: this.incidentDescription,
+        incidentType: {
+          name: this.incidentTypeName,
+          description: "Type 1 description 1"
+        },
+        address: "Address 1",
+        location: {
+          latitude: this.latMarker,
+          longitude: this.lngMarker
+        },
+        creationDate: creationDate,
+        expirationDate: expirationDate,
+        dangerLevel: 10,
+      };
+      this.incidentService.addIncident(incident);
+    }    
 }
